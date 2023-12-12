dbf30706702720c8dd4fb007e3622eb9ffe5ef75
Rohan-Mainali <rohanmainali39@gmail.com>
2023-12-12 19:30:23 +0545

    pointer in functions

--- pointers/basic.go
+++ pointers/basic.go
@@ -2,6 +2,25 @@ package pointers
 
 import "fmt"
 
+type Creature struct {
+	Species string
+}
+
+func changeCreature(creature *Creature) {
+	creature.Species = "jellyfish"
+	fmt.Printf("2) %+v\n", creature)
+}
+
+func add(a int32, b int32) int32 {
+	a += 2
+	return a + b
+}
+
+func addByReference(a *int32, b int32) int32 {
+	*a += 2
+	return *a + b
+}
+
 func Pointer() {
 	var name string = "rohan"
 	fmt.Println("Initial Name")
@@ -15,4 +34,16 @@ func Pointer() {
 
 	fmt.Println("----Value Name----")
 	fmt.Println(name3)
+
+	var firstNum int32 = 3
+	var secondNum int32 = 5
+	ans := addByReference(&firstNum, secondNum)
+	fmt.Println(ans)
+	fmt.Println("first  ", firstNum)
+
+	var creature Creature = Creature{Species: "shark"}
+
+	fmt.Printf("1) %+v\n", creature)
+	changeCreature(&creature)
+	fmt.Printf("3) %+v\n", creature)
 }
